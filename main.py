5d1c983db30ccd431d3e0b5b457a278fe1253b52
arditsulce <europe.gov.eu@gmail.com>
2020-11-23 18:22:13 +0100

    Add translator

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from greets import greetings
+from translate import Translator
+
+translator = Translator(to_lang='pt')
 
 for g in greetings:
-    print(g.title())
+    print(translator.translate(g).title())
